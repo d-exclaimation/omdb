59fbdc989ffb8de1cbf2c8fd5d72506acaad0b76
Vincent <thisoneis4business@gmail.com>
2023-05-15 14:41:52 +1200

    fix: Fixed zip command again

--- zip.sh
+++ zip.sh
@@ -4,29 +4,6 @@
 if [ ! -f "seng365-assignment2-omdb-vno16.zip" ]; then
     rm seng365-assignment2-omdb-vno16.zip
 fi
-if [ ! -d "temp" ]; then
-    rm -rf temp
-fi
-mkdir temp
-
-# Copy files
-cp -r src temp/src
-cp -r public temp/public
-cp package.json temp/package.json
-cp package-lock.json temp/package-lock.json
-cp .env temp/.env
-cp index.html temp/index.html
-cp postcss.config.js temp/postcss.config.js
-cp tailwind.config.js temp/tailwind.config.js
-cp tsconfig.json temp/tsconfig.json
-cp tsconfig.node.json temp/tsconfig.node.json
-cp vite.config.ts temp/vite.config.ts
-cp .gitignore temp/.gitignore
 
 # Zip
-cd temp
-zip -r ../seng365-assignment2-omdb-vno16.zip *
-cd ..
-
-# Remove temp
-rm -rf temp
+zip -r seng365-assignment2-omdb-vno16.zip src public package*.json .env index.html postcss.config.js tailwind.config.js tsconfig.json tsconfig.node.json vite.config.ts .gitignore
