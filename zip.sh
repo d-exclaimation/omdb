1a01fe44052722f99078447a8446bee4e808a1d8
Vincent <thisoneis4business@gmail.com>
2023-05-15 14:33:20 +1200

    feat: Auto zip

---
+++ zip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Remove old zip
+if [ ! -f "seng365-assignment2-omdb-vno16.zip" ]; then
+    rm seng365-assignment2-omdb-vno16.zip
+fi
+if [ ! -d "temp" ]; then
+    rm -rf temp
+fi
+mkdir temp
+
+# Copy files
+cp -r src temp/src
+cp -r public temp/public
+cp package.json temp/package.json
+cp package-lock.json temp/package-lock.json
+cp .env temp/.env
+cp index.html temp/index.html
+cp postcss.config.js temp/postcss.config.js
+cp tailwind.config.js temp/tailwind.config.js
+cp tsconfig.json temp/tsconfig.json
+cp tsconfig.node.json temp/tsconfig.node.json
+cp vite.config.ts temp/vite.config.ts
+cp .gitignore temp/.gitignore
+
+# Zip
+zip -r seng365-assignment2-omdb-vno16.zip temp
+
+# Remove temp
+rm -rf temp
