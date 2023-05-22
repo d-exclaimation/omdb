655798d4102636f3cb1f060e3f5f6c7ae0a45143
Vincent <thisoneis4business@gmail.com>
2023-05-23 10:34:13 +1200

    feat: Update the zip file to use the correct name

--- zip.sh
+++ zip.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # Remove old zip
-if [ ! -f "seng365-assignment2-omdb-vno16.zip" ]; then
-    rm seng365-assignment2-omdb-vno16.zip
+if [ ! -f "vno16.zip" ]; then
+    rm vno16.zip
 fi
-if [ ! -d "seng365-assignment2-omdb-vno16" ]; then
-    rm -rf seng365-assignment2-omdb-vno16
+if [ ! -d "vno16" ]; then
+    rm -rf vno16
 fi
-mkdir seng365-assignment2-omdb-vno16
+mkdir vno16
 
 # Copy files
-cp -r src seng365-assignment2-omdb-vno16/src
-cp -r public seng365-assignment2-omdb-vno16/public
-cp package.json seng365-assignment2-omdb-vno16/package.json
-cp package-lock.json seng365-assignment2-omdb-vno16/package-lock.json
-cp .env seng365-assignment2-omdb-vno16/.env
-cp index.html seng365-assignment2-omdb-vno16/index.html
-cp postcss.config.js seng365-assignment2-omdb-vno16/postcss.config.js
-cp tailwind.config.js seng365-assignment2-omdb-vno16/tailwind.config.js
-cp tsconfig.json seng365-assignment2-omdb-vno16/tsconfig.json
-cp tsconfig.node.json seng365-assignment2-omdb-vno16/tsconfig.node.json
-cp vite.config.ts seng365-assignment2-omdb-vno16/vite.config.ts
-cp .gitignore seng365-assignment2-omdb-vno16/.gitignore
+cp -r src vno16/src
+cp -r public vno16/public
+cp package.json vno16/package.json
+cp package-lock.json vno16/package-lock.json
+cp .env vno16/.env
+cp index.html vno16/index.html
+cp postcss.config.js vno16/postcss.config.js
+cp tailwind.config.js vno16/tailwind.config.js
+cp tsconfig.json vno16/tsconfig.json
+cp tsconfig.node.json vno16/tsconfig.node.json
+cp vite.config.ts vno16/vite.config.ts
+cp .gitignore vno16/.gitignore
 
 # Zip
-zip -r seng365-assignment2-omdb-vno16.zip seng365-assignment2-omdb-vno16
+zip -r vno16.zip vno16
 
 # Remove temp
-rm -rf seng365-assignment2-omdb-vno16
+rm -rf vno16
